f648cb08e1d496794b1f51a351a669fdc0809ac8
Matt Parsons <maparsons83@yahoo.com>
2018-03-23 11:23:56 -0400

    finished

--- script.js
+++ script.js
@@ -5,30 +5,30 @@ let mixArray = [1, 3, 5, 3, 7, 3, 1, 1, 5];
 
 // 1. Reverse a string
 
-function One(string) {
-    let reverseWords = testString.split("").reverse().join("");
+function One(kata1String) {
+    let reverseWords = kata1String.split("").reverse().join("");
     return reverseWords;
 }
 
 // 2. Reverse a sentence ("bob likes dogs" -> "dogs likes bob")
 
-function Two(string) {
-    let reverseString = testString.split(",").reverse();
+function Two(kata2String) {
+    let reverseString = kata2String.split(",").reverse();
     let testString1 = JSON.stringify(reverseString);
     return testString1;
 }
 
 // 3. Find the minimum value in an array
 
-function Three(array) {
-    let minArray = Math.min(...numArray);
+function Three(kata3Array) {
+    let minArray = Math.min(...kata3Array);
     return minArray;
 }
 
 // 4. Find the maximum value in an array
 
-function Four(array) {
-    let maxArray = Math.max(...numArray);
+function Four(kata4Array) {
+    let maxArray = Math.max(...kata4Array);
     return maxArray;
 }
 
@@ -42,12 +42,12 @@ function Five(x, y) {
 
 // 6. Return distinct values from a list including duplicates (i.e. "1 3 5 3 7 3 1 1 5" -> "1 3 5 7")
 
-function Six(array) {
+function Six(kata6Array) {
     // let unique = [...new Set(mixArray)]; // Array.from(new Set(mixArray))
     // console.log(unique);
 
     let testObject = {};
-    for (let unique of mixArray) {
+    for (let unique of kata6Array) {
         if (!testObject[unique]) {
             testObject[unique] = true;
         }
@@ -58,17 +58,44 @@ function Six(array) {
 
 // 7. Return distinct values and their counts (i.e. the list above becomes "1(3) 3(3) 5(2) 7(1)")
 
-function Seven(array) {
+function Seven(kata7Array) {
     let counts = {};
-    for (let unique of mixArray) {
+    for (let unique of kata7Array) {
         counts[unique] = (counts[unique] || 0) + 1;
     }
     return counts
 }
 
+function eight(passString, passObject) {
+    
+
+    let obString = passString;
+    let obValues = passObject;
+    let total = 0;
+    let obArray = obString.split(" ");
+    
+    for(let i = 0; i < obArray.length; i++) {
+        obResults = obArray[i];
+        if(obResults === "+" || obResults === "-") {
+            continue;
+            
+        } else {
+            let letter = obArray[i];
+            let operators = obArray[i-1];
+            if (operators === "+" || operators === undefined) {
+                total += obValues[letter];
+            } else if (operators === "-") {
+                total -= obValues[letter];
+                
+            }
+            
+        }
+        
+    }
+    return total;
+} 
 
 
-// console.log(unique);
 
 
 
@@ -94,7 +121,30 @@ function Seven(array) {
     const kata3Result1 = Three([6,9,15,12]);
     const kata3Result2 = Three([100,201,88,1000]);
 
-    console.assert(kata3Result1 === '[6]', kata3Result1);
+    console.assert(kata3Result1 === 6, kata3Result1);
+    console.assert(kata3Result2 === 88, kata3Result2);
+
+// Kata 4: 
+    const kata4Result1 = Four([6,9,15,12]);
+    const kata4Result2 = Four([100,201,88,1000]);
+
+    console.assert(kata4Result1 === 15, kata4Result1);
+    console.assert(kata4Result2 === 1000, kata4Result2);
+
+// Kata 5: 
+    const kata5Result1 = Five(12,4);
+    const kata5Result2 = Five(60,10);
+
+    console.assert(kata5Result1 === 3, kata5Result1);
+    console.assert(kata5Result2 === 6, kata5Result2);
+
+// Kata 6:
+    const kata6Result1 = Six([1,2,2]);
+    const kata6Result2 = Six([8,8,8,5,1]);
+
+    console.assert(JSON.stringify(kata6Result1 === `{"1","2"}`, kata6Result1));
+    console.assert(JSON.stringify(kata6Result2 === `{"1","5","8"}`, kata6Result1));
+
 
 // Kata 7: 
     const kata7Result1 = Seven([1,2,2]);
@@ -103,12 +153,20 @@ function Seven(array) {
     console.assert(JSON.stringify(kata7Result1) === `{"1":1,"2":2}`, kata7Result1);
     console.assert(JSON.stringify(kata7Result2) === `{"1":1,"5":1,"8":3}`, kata7Result2);
 
-console.log("One:", One());
-console.log("Two:", Two());
-console.log("Three:", Three());
-console.log("Four:", Four());
-console.log("Five:", Five(4,2));
-console.log("Six:", Six());
-console.log("Seven:", Seven());
+// Kata 8: 
 
-// console.log("One2:", One("blue"));
+    const kata8Result1 = eight("a - d", {a:5, d:3});
+    const kata8Result2 = eight("b + c - a", {a:8, b:6, c:4});
+
+    console.assert(kata8Result1 === 2, kata8Result1);
+    console.assert(kata8Result2 === 2, kata8Result2);
+
+console.log("One:", One(testString));
+console.log("Two:", Two(testString));
+console.log("Three:", Three(numArray));
+console.log("Four:", Four(numArray));
+console.log("Five:", Five(4,2));
+console.log("Six:", Six(mixArray));
+console.log("Seven:", Seven(mixArray));
+console.log("Eight:", eight("a + b + c - d", {a: 1, b: 7, c: 3, d: 14}));
+
